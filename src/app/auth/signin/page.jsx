d4740290f75ba91f7628a8733685296418ea9cd5
khalid66527 <khalidhasan678954321@gmail.com>
2026-05-03 16:06:38 +0600

    set up this project

---
+++ src/app/auth/signin/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignInPage = () => {
+    return (
+        <div>
+            <h1>This is SignIn Page</h1>
+            
+        </div>
+    );
+};
+
+export default SignInPage;
